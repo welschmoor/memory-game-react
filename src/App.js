ae72026d2dcac8b9f580f474c560509bcc5c4659
Johannes Welsch <welschmoor@gmail.com>
2021-12-07 07:40:25 +0100

    app finished

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react"
-import styled, { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle, ThemeProvider } from "styled-components";
 import Mem from "./components/Mem";
 
 const GlobalStyle = createGlobalStyle`
@@ -16,70 +16,155 @@ const GlobalStyle = createGlobalStyle`
   }
 `
 
+const lightTheme = {
+
+}
+
+const darkTheme = {
+  bgCol: "#252424",
+  tileBgCol: "grey",
+  textCol: "white",
+  titleCol: '#b9b9b9',
+  subtitleCol: "#969696",
+}
+
 
 const mems = [
-  { value: 1, name: "1a", open: false, match: false },
-  { value: 1, name: "1b", open: false, match: false },
-  { value: 1, name: "1c", open: false, match: false },
+  { value: 1, name: "1a", open: false, match: false, clicked: false },
+  { value: 1, name: "1b", open: false, match: false, clicked: false },
+  { value: 1, name: "1c", open: false, match: false, clicked: false },
 
-  { value: 2, name: "2a", open: false, match: false },
-  { value: 2, name: "2b", open: false, match: false },
-  { value: 2, name: "2c", open: false, match: false },
+  { value: 2, name: "2a", open: false, match: false, clicked: false },
+  { value: 2, name: "2b", open: false, match: false, clicked: false },
+  { value: 2, name: "2c", open: false, match: false, clicked: false },
 
-  { value: 3, name: "3a", open: false, match: false },
-  { value: 3, name: "3b", open: false, match: false },
-  { value: 3, name: "3c", open: false, match: false },
+  { value: 3, name: "3a", open: false, match: false, clicked: false },
+  { value: 3, name: "3b", open: false, match: false, clicked: false },
+  { value: 3, name: "3c", open: false, match: false, clicked: false },
 
-  { value: 4, name: "4a", open: false, match: false },
-  { value: 4, name: "4b", open: false, match: false },
-  { value: 4, name: "4c", open: false, match: false },
+  { value: 4, name: "4a", open: false, match: false, clicked: false },
+  { value: 4, name: "4b", open: false, match: false, clicked: false },
+  { value: 4, name: "4c", open: false, match: false, clicked: false },
 
 ]
 
 
 function App() {
-  const [memState, setMemState] = useState()
-  const [firstState, setFirstState] = useState(false)
-  const [secondState, setSecondState] = useState(false)
-  const [thirdState, setThirdState] = useState(false)
+  const [memState, setMemState] = useState([])
+  const [timesClicked, setTimesClicked] = useState(0)
+  const [moves, setMoves] = useState(0)
+  const [darkMode, setDarkMode] = useState(false)
 
-  useEffect(()=> {
-    const memsSorted = [...mems].sort(()=> Math.random()-0.5)
-    setMemState(memsSorted)
+  useEffect(() => {
+    newGameBtnHandler()
   }, [])
 
+  const newGameBtnHandler = () => {
+    const memsSorted = [...mems].sort(() => Math.random() - 0.5)
+    setMemState(memsSorted)
+    setTimesClicked(0)
+    setMoves(0)
+  }
 
   const clickHandler = (name) => {
+    if (timesClicked === 3) return // this prevents the 4th tile from openening!
+    if (memState.find(e => e.name === name).clicked) return; // this prevents from clicking the same tile twice or more times
 
-    const clickedMem = memState.filter(e=> e.name===name)
-    console.log("clickedMem", clickedMem)
-    setMemState(p=>{ 
-      console.log(p)
-      const stateWithoutOne = p.filter( e => e.name !== name )
+    setMemState(p => {  // this code sets clicked to true to prevent double clicks on the same tile
+      return p.map(e => {
+        if (e.name === name) {
+          return { ...e, clicked: true }
+        } else {
+          return e
+        }
+      })
+    })
 
-      return [...clickedMem, ...stateWithoutOne]
+    setTimesClicked(p => p + 1)
+
+    setMemState(p => {  // clicked tile has its open prop set to true
+      return p.map(e => {
+        if (e.name === name) {
+          return { ...e, open: true }
+        } else {
+          return e
+        }
+      })
     })
   }
 
+  // what do we do when three cards match
+  useEffect(() => {
+    
+    if (timesClicked === 3) { // the reason 3 is we have three equal tiles
+      setMoves(p=>p+1)
+      let tempArr = [];
+      memState.filter(e => e.open).forEach(e => {
+        tempArr.push(e.value)
+      })
+
+      const won = tempArr.every(val => val === tempArr[0]);
+
+      if (won) {
+        console.log("Match!!!")
+        setTimesClicked(0)
+        setMemState(p => {
+          return p.map(e => {
+            if (e.open) {
+              return { ...e, match: true, open: false }
+            } else {
+              return { ...e, open: false }
+            }
+          })
+        })
+      } else { //if won === false        
+        setTimeout(() => {
+          setMemState(p => {
+            return p.map(e => {
+              return { ...e, open: false, clicked: false }
+            })
+          })
+          setTimesClicked(0)
+        }, 1337)
+      }
+    }
+  }, [timesClicked]) // /end useEffect
+
+
+
   return (
-    <Wrapper>
-      <GlobalStyle />
-      <GRID >
-        {memState && memState.map(e => {
-          return (
-            <Mem {...e} onClick={clickHandler} hidden={e.open} key={e.name}/>
-          )
-        })}
-      </GRID>
-    </Wrapper>
+    <ThemeProvider theme={darkTheme} >
+      <Wrapper>
+        <GlobalStyle />
+        <div>
+          <Title>Memory Game</Title>
+          <Subtitle>Find 3 matching numbers!</Subtitle>
+        </div>
+        <GRID >
+          {memState && memState.map(e => {
+            return (
+              <Mem {...e} onClick={clickHandler} hidden={e.open} key={e.name} />
+            )
+          })}
+        </GRID>
+        <Moves>Moves: {moves}</Moves>
+        <NewGameBtn onClick={newGameBtnHandler} >New Game</NewGameBtn>
+      </Wrapper>
+    </ThemeProvider>
   );
 }
 
 
 const Wrapper = styled.div`
-  margin: 80px auto;
-
-`
+  padding-top: 30px;
+  margin: 0px auto;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+  background-color: ${p => p.theme.bgCol};
+  `
 
 const GRID = styled.div`
   display: grid;
@@ -92,4 +177,29 @@ const GRID = styled.div`
   position: relative;
 `
 
+const Title = styled.h2`
+  color: ${p=>p.theme.titleCol};
+  font-size: 2rem;
+  display: flex;
+  justify-content: center;
+`
+
+const Subtitle = styled(Title)`
+  font-size: 1.1rem;
+  color: ${p=>p.theme.subtitleCol};
+`
+
+const Moves = styled.h4`
+  font-size: 1.3rem;
+  color: ${p=>p.theme.subtitleCol};
+`
+
+const NewGameBtn = styled.button`
+  border: 2px solid ${p=>p.theme.subtitleCol};
+  cursor: pointer;
+  background-color: ${p=>p.theme.bgCol};
+  color: ${p=>p.theme.titleCol};
+  padding: 10px 20px;
+  font-size: 1rem;
+`
 export default App;
--- src/components/Mem.js
+++ src/components/Mem.js
@@ -1,35 +1,27 @@
 import React from "react"
-
 import styled from "styled-components"
-
 
 
 const Mem = ({ value, name, open, match, onClick }) => {
 
-
-
-
   return (
     <>
       <ValueWrapper onClick={() => onClick(name)} >
-        <div style={ { transform: open || match ? `rotateY(0deg)` : `rotateY(70deg)` } }>{value}</div>
-
+        <div style={ { transform: open || match ? `rotateY(0deg)` : `rotateY(90deg)` } }>{value}</div>
         <HidePlane />
       </ValueWrapper>
     </>
   )
 }
 
-
-
 const ValueWrapper = styled.div`
   height: 160px;
   width: 160px;
-  background-color: grey;
+  background-color: ${p=>p.theme.tileBgCol};
   display: flex;
   align-items: center;
   justify-content: center;
-  color: white;
+  color: ${p=>p.theme.textCol};
   font-size: 5rem;
   cursor: pointer;
 `
@@ -37,10 +29,9 @@ const ValueWrapper = styled.div`
 const HidePlane = styled.div`
   height: 160px;
   width: 160px;
-  background-color: grey;
+  background-color: ${p=>p.theme.tileBgCol};
   position: absolute;
   z-index: -1;
 `
-
 
 export default Mem
